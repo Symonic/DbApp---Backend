0748df2e0bb2c03121c59cfa211b7cc83eda4ffa
Symonic <93929565+Symonic@users.noreply.github.com>
2024-04-08 20:40:53 +0200

    JWT Setup

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from main.views import login
+from main.views import HomeView, LogoutView
+
+#AUTH
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('login/', login)
+    path('home/', HomeView.as_view(), name = 'home'),
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name = 'token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name = 'token_refresh'),
+    path('logout/', LogoutView.as_view(), name = 'logout')
 ]
